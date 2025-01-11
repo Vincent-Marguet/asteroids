d0a2fae7c3fa9a70ce766210f3cdfa20644f91e1
Vincent-Marguet <vimdia.business@gmail.com>
2025-01-11 10:28:49 +0100

    Started Project Asteroids
    - created main.py: we will be using pygame lib
    - created constants.py: to hold a few constants
    - created .gitignore file : ignoring venv/ and __pycache__/
    - created requirements.txt

---
+++ main.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+
+import pygame
+
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+
+
+def main():
+    """
+    Main function of Asteroids game
+    """
+    print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill("black")
+        pygame.display.flip()
+
+
+if __name__ == "__main__":
+    main()
