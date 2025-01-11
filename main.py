6c14f66682b7eba8d1cee51307ca008e36b46f1b
Vincent-Marguet <vimdia.business@gmail.com>
2025-01-11 11:47:15 +0100

    Fix in main.py
    - fixed clock.tick (float instead of int)
    - fixed call to Clock constructore in pygame.time module

--- main.py
+++ main.py
@@ -21,12 +21,16 @@ def main():
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        clock.tick(60.00)
+        dt = clock.tick(60.00) / 1000
 
 
 if __name__ == "__main__":
