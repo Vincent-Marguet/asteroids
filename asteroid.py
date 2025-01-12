99c0c8b4bfa4c945cd16d90a987671238d80047e
Vincent-Marguet <vimdia.business@gmail.com>
2025-01-12 07:05:31 +0100

    Implement asteroids and asteroid fields
    WORK:
    
    - add AsteroidField class in asteroidfield.py
    - add Asteroid Class in asteroid.py
    - rename other groups in main.py
    - add asteroids group in main.py.
    
    TEST:
    - ran application 5 times :
    asteroids are there, movements are good, exit is clean.

---
+++ asteroid.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env -S python3 -i
+"""
+This is the module for Asteroid class
+"""
+
+import pygame
+
+from circleshape import CircleShape
+
+
+class Asteroid(CircleShape):
+    """
+    Asteroid class
+    """
+
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+        self.x = x
+        self.y = y
+        self.radius = radius
+
+    def draw(self, screen):
+        """
+        Overrides the CircleShape draw method.
+        Draw the asteroid.
+        """
+        pygame.draw.circle(
+            surface=screen,
+            color="white",
+            center=self.position,
+            radius=self.radius,
+            width=2,
+        )
+
+    def update(self, dt):
+        self.position += self.velocity * dt
--- main.py
+++ main.py
@@ -6,6 +6,8 @@
 
 import pygame
 
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from player import Player
 
@@ -25,21 +27,25 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
 
-    updatable_group = pygame.sprite.Group()
-    drawable_group = pygame.sprite.Group()
+    updatables = pygame.sprite.Group()
+    drawables = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
 
-    Player.containers = (updatable_group, drawable_group)
+    AsteroidField.containers = updatables
+    Asteroid.containers = (updatables, drawables, asteroids)
+    Player.containers = (updatables, drawables)
 
     player_1 = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    asteroic_field_1 = AsteroidField()
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        for updatable in updatable_group:
+        for updatable in updatables:
             updatable.update(dt)
-        for drawable in drawable_group:
+        for drawable in drawables:
             drawable.draw(screen)
         pygame.display.flip()
         clock.tick(60.00)
